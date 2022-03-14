bf31fbd0521a4f74b972e66736e277d68a311850
Mick Butler <80526334+mickbut-ler@users.noreply.github.com>
2022-03-14 18:23:11 +0100

    Created header workflow

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import NavigationMenu from "./NavigationMenu";
 
 const Header = () => {
   return (
-    <div className="nav">
+    <div className="header__section">
       <Slider />
       <NavigationMenu />
     </div>
@@ -12,3 +12,10 @@ const Header = () => {
 };
 
 export default Header;
+
+// Workflow header section:
+//  1. Set height and with of the header and set all div elements:
+//    1.1 Burger menu, STOX logo, shopping basket(FontAwesome)
+//  2. Organize div
+//  3. Burger menu when clicked show X menu
+//  4. When clicked is active show "Slider & NavigationMenu"
