c31f3f10a51ac9cf7290b8ec23c609b7ece75099
Mick Butler <80526334+mickbut-ler@users.noreply.github.com>
2022-03-15 15:15:18 +0100

    3. Burger menu opening & closing onClick()

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "./Slider";
 import NavigationMenu from "./NavigationMenu";
 
@@ -11,39 +11,42 @@ import {
   faShoppingBag,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Header = () => {
-  let clicked = false;
-  let burgerMenu = (clicked = true ? (
-    <FontAwesomeIcon icon={faBars} />
-  ) : (
-    <FontAwesomeIcon icon={faXmark} />
-  ));
-
-  // const logo
-
-  return (
-    <div className="">
-      <div className="header__section">
-        <div className="header__left">
-          {burgerMenu}
-          <img src="stox_logo.png" className="stox__logo" alt="" />
-        </div>
-        <div className="header__right">
-          <FontAwesomeIcon icon={faShoppingBag} />
-        </div>
-      </div>
-
-      <Slider />
-      <NavigationMenu />
-    </div>
-  );
-};
-
-export default Header;
-
 // Workflow header section:
 //  1. Set height and with of the header and set all div elements:
 //    1.1 Burger menu, STOX logo, shopping basket(FontAwesome)
 //  2. Organize div
 //  3. Burger menu when clicked show X menu
 //  4. When clicked is active show "Slider & NavigationMenu "
+
+function Header() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  function magnifyNavbar() {
+    setIsClicked(!isClicked);
+    console.log(isClicked);
+  }
+
+  let burgerMenu = isClicked == false ? faBars : faXmark;
+
+  return (
+    <div className="">
+      <div className="header__section">
+        <div className="header__left">
+          <FontAwesomeIcon
+            icon={burgerMenu}
+            onClick={magnifyNavbar}
+            className="close__header"
+          />
+          <img src="stox_logo.png" className="stox__logo" alt="" />
+        </div>
+        <div className="header__right">
+          <FontAwesomeIcon icon={faShoppingBag} />
+        </div>
+      </div>
+      <Slider />
+      <NavigationMenu />
+    </div>
+  );
+}
+
+export default Header;
