649627c4e52f322a4de60c84cd6f5693b60d2eea
Mick Butler <80526334+mickbut-ler@users.noreply.github.com>
2022-03-15 15:51:04 +0100

    Slider - workflow done

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Slider = () => {
-  return <div>Slider</div>;
+  return (
+    <div className="slider">
+      <h1>hello</h1>
+    </div>
+  );
 };
 
 export default Slider;
+
+// Workflow slider section:
+//  1. Set div with width and test if it is scrolling horizontal.
+//  2. Create data object containing images url and title.
+//  3. Map over data object returning image component passing the right data.
+//  4. Remove with from test.
