c5a074aa4409c3b3f3ec1c28167624674790b95c
Mick Butler <80526334+mickbut-ler@users.noreply.github.com>
2022-03-16 12:33:25 +0100

    DropdownMenu - added workflow for onCLick()

--- src/components/DropDownMenu.js
+++ src/components/DropDownMenu.js
@@ -7,10 +7,14 @@ import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
 
 const DropdownMenu = () => {
   const angleDaily = true ? faAngleUp : faAngleDown;
+
+  function toggleListItems() {
+    console.log("toggleListItems");
+  }
   return (
     <div>
       <section>
-        <div className="dropdown__header">
+        <div onClick={toggleListItems} className="dropdown__header">
           <h3>DAILY</h3>
           <FontAwesomeIcon icon={angleDaily} className="angle__icon" />
         </div>
@@ -31,4 +35,8 @@ export default DropdownMenu;
 //  1. Create two divs with header and li items
 //  2. Style li items and header
 //  3. onCLick() function to toggle li of div
+//    3.1 one function plays with two variables toggleDaily and toggleSports
+//    3.2 start both false
+//    3.3 If toggleDaily == true ToggleSports == false
+//    3.4 visa versa
 //  4. Ensure that when clicked on heading div1 li div2 should collapse
