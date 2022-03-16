59a916ddfbb61341400dd2a7410a9f85506064da
Mick Butler <80526334+mickbut-ler@users.noreply.github.com>
2022-03-16 11:57:30 +0100

    DropdownMenu 1.0 - created div with heading, icon & li

--- src/components/DropDownMenu.js
+++ src/components/DropDownMenu.js
@@ -1,13 +1,33 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
+
 const DropDownMenu = () => {
-  return <div>NavigationMenu</div>;
+  const angleDaily = true ? faAngleUp : faAngleDown;
+  return (
+    <div>
+      <div className="dropdown__header">
+        <h3>Daily</h3>
+        <FontAwesomeIcon icon={angleDaily} className="close__header" />
+      </div>
+      <ul>
+        <li>test</li>
+      </ul>
+      {/* <div className="dropdown__header">
+        <h3>Sports</h3>
+      </div>
+      <ul>
+        <li>test</li>
+      </ul> */}
+    </div>
+  );
 };
 
 export default DropDownMenu;
 
 // Workflow DropdownMenu section:
 //  1. Create two divs with header and li items
-//  2. Style li items
+//  2. Style li items and header
 //  3. onCLick() function to toggle li of div
 //  4. Ensure that when clicked on heading div1 li div2 should collapse
